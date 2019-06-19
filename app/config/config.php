3a9c54eb7922c270b220e0a7a68296547483cba5
noe nevarez <closner_nevarez@hotmail.com>
2019-06-19 14:11:33 +0700

    pushing files

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'vagrant');
+  define('DB_NAME', 'newapp_db');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/sharepost/');
+  // Site Name
+  define('SITENAME', 'SharePost');
+  // App Version
+  define('APPVERSION', '1.0.0');
